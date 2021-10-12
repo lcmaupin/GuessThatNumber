f3ea334a40731b81a445e7766571dbdca1742dbd
Luke Maupin <lukecmaupin@gmail.com>
2021-10-12 10:13:13 -0500

    Adds variables to guess.js

---
+++ guess.js
@@ -0,0 +1,22 @@
+// GUESS THAT NUMBER
+
+// Message to be used throughout file
+const enterNumText = "Please enter a number greater than zero";
+
+// Used to restart the game
+let restartGame = true;
+
+// Stores range of numbers guessed
+let rangeNum;
+
+// Stores random number to be guessed
+let randomNum;
+
+// Stores number of attempts user has remaining
+let attempts;
+
+// Stores user's guessed number
+let guess;
+
+//  Stores user's response to play again prompt
+let playAgain;
