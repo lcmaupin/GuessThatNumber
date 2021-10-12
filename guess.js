d81677acdf79cf5fa11c3f3c731b7508cc8f7120
Luke Maupin <lukecmaupin@gmail.com>
2021-10-12 12:34:47 -0500

    Adds attempts section code

--- guess.js
+++ guess.js
@@ -26,15 +26,30 @@ alert(`Welcome to the "GUESS THAT NUMBER!" Please click "OK" to start the game.`
 
 // 
 while (restartGame){
-    // Prompts user to enter number setting upper bound for random number to be created
-    // Captures user entry in rangeNum variable
+    // Prompts user to enter number setting upper bound for random number to be created and captures user entry in rangeNum variable
     rangeNum = prompt(`Please enter a maximum number for the range:`);
+
     // Converts rangeNum from string to number. If value cannot be parsed, returns NaN
     rangeNum = parseInt(rangeNum);
+
     // Verifies the entry for rangeNum is greater than zero. NaN has a boolean value of false. All non-zero numbers have a boolean value of true; zero has a value of false
     while (!rangeNum || rangeNum < 1){
         rangeNum = prompt(enterNumText);
         rangeNum = parseInt(rangeNum);
     }
+
+    // Creates random number to be guessed
+    randomNum = Math.floor(Math.random() * rangeNum) + 1;
+
+    // Prompts user to enter the number of guesses to allow
+    attempts = parseInt(prompt(`Please enter the number of attempts allowed:`));
+
+    // Verifies that the value for attempts is greater than zero
+    while (!attempts || attempts < 1){
+        attempts = parseInt(prompt(enterNumText));
+    }
+
+    // 
+
     break;
 }
